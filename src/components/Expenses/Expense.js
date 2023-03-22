ec59b6ecde03da0680c509a7540feadcdf3a3188
Phoo Phoo <zinphoomon.zpm@gmail.com>
2023-03-22 09:09:38 +0630

    bmps online testing

---
+++ src/components/Expenses/Expense.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expense.css";
+
+const Expense = (props) => {
+  const [filterYear, setFilterYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
+  const filterExpenses = props._expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
+  // let expensesContent = <p>No Found Item</p>;
+  // if (filterExpenses.length > 0) {
+  //   expensesContent = filterExpenses.map((expense) => {
+  //     return (
+  //       <ExpenseItem
+  //         key={expense.id}
+  //         title={expense.title}
+  //         amount={expense.amount}
+  //         date={expense.date}
+  //       />
+  //     );
+  //   });
+  // }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filterYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesChart expenses={filterExpenses}/>
+      <ExpensesList items={filterExpenses} />;
+      {/* {filterExpenses.length === 0 && expensesContent}
+        {filterExpenses.length > 0 && expensesContent} */}
+      {/* {props._expenses.map((expense)=> {
+          return(
+            <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+        />
+          );
+        })} */}
+      {/* <ExpenseItem
+        title={props._expenses[0].title}
+        amount={props._expenses[0].amount}
+        date={props._expenses[0].date}
+        />
+        <ExpenseItem
+            title={props._expenses[1].title}
+            amount={props._expenses[1].amount}
+            date={props._expenses[1].date}
+        />
+        <ExpenseItem
+            title={props._expenses[2].title}
+            amount={props._expenses[2].amount}
+            date={props._expenses[2].date}
+        />
+        <ExpenseItem
+            title={props._expenses[3].title}
+            amount={props._expenses[3].amount}
+            date={props._expenses[3].date}
+        /> */}
+    </Card>
+  );
+};
+export default Expense;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  // const [title, subtitle] = useState(props.title); // let title = props.title;
+
+  // const clickResult = () => {
+  //     //title = "Update";
+  //     subtitle("Update !!!");
+  // };
+
+  // const expenseDate = new Date(2023, 3, 10);
+  // const expenseTitle = "Buy New Car";
+  // const expenseAmount = 90000;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        {/* <h2>{props.title}</h2> */}
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+
+      {/* <button onClick={() => {
+                console.log("Clicked");
+            }}>Change</button> */}
+
+      {/* <button onClick={clickResult}>Change Title</button> */}
+
+      {/* <div>10 - 3 - 2023</div>
+            <div className="expense-item__description">
+                <h2>Buy Car</h2>                
+                <div className="expense-item__price">$500000</div>
+            </div> */}
+
+      {/* <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div> */}
+    </Card>
+  );
+};
+
+export default ExpenseItem;
